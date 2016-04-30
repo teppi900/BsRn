463292dcc1ba9ba180eaf418be55a96e687a551b
kid <kid@DESKTOP-SM1L6FN.lan>
2016-04-30 19:06:23 +0200

    re:re:new

--- BsRn/src/portfolio4/Scheduling.java
+++ BsRn/src/portfolio4/Scheduling.java
@@ -22,34 +22,21 @@ public class Scheduling {
 		int[]ankunftsZeit=new int[sizes.size()];
 		for (int i = 0; i < ankunftsZeit.length; i++) {
 			ankunftsZeit[i]=sizes.get(i).getAnkuftsZeit();
-//			System.out.println(ankunftsZeit[i]);
 		}
-//		System.out.println(ankunftsZeit.length);
 		int[]laufZeit=new int[sizes.size()];
 		for (int i = 0; i < laufZeit.length; i++) {
 			laufZeit[i]=sizes.get(i).getLaufZeit();
-//			System.out.println(laufZeit[i]);
 		}
-//		System.out.println(laufZeit.length);
 		finishedTimes[0] = ankunftsZeit[0] + laufZeit[0];
 		System.out.println(finishedTimes[0]);
 		for (int k = 1; k < sizes.size(); k++) {
 			if (ankunftsZeit[k]<=finishedTimes[k-1]) {
 				finishedTimes[k] = finishedTimes[k - 1] + laufZeit[k];
-//				System.out.print(finishedTimes[k]+"="+finishedTimes[k-1]+"+"+laufZeit[k]);
+
 			}else {
 				finishedTimes[k]=ankunftsZeit[k]+laufZeit[k];
-//				System.out.print(finishedTimes[k]+"="+k+"+"+laufZeit[k]);
 			}
-//		     System.out.println(finishedTimes[k]);
 		}	
-//		System.out.println(finishedTimes.length);
-//		int temp2=sizes.get(0).getAnkuftsZeit();//Länge der leerspace
-//		int temp=0;
-//		int ankunft=sizes.get(0).getAnkuftsZeit();									//gesamt lange
-//		for (int i = 0; i < sizes.size(); i++) {
-//			ankunft=ankunft+sizes.get(i).getLaufZeit();
-//		}
 		int length=finishedTimes[finishedTimes.length-1];
 		for (int i = 0; i <length ; i++) {
 			if (i<10) {
@@ -58,12 +45,6 @@ public class Scheduling {
 				System.out.print("\t"+i);
 			}
 		}
-//		for (int i = 0; i < ankunft; i++) {
-//			System.out.print("  "+i);
-//			if (i==ankunft-1) {
-//				System.out.print("\n");
-//			}
-//		}
 		int temp=ankunftsZeit[0];
 		int freeTime=0;	
 		for (int i = 0; i < sizes.size(); i++) {
@@ -81,8 +62,8 @@ public class Scheduling {
 			}
 				for (int j = 0; j < laufZeit[i]; j++) {											//Laufzeit
 					System.out.print("*"+"\t");																
-				}																			//wenn fertig Laufzeit zum temp addieren
-				temp+=laufZeit[i];
+				}																				//wenn fertig Laufzeit zum temp addieren
+					temp+=laufZeit[i];
 		}
 	}
 	public void SRTF(){
